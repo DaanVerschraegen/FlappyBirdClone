aead5d9f7fe4afa0af66054d61b87e72158daa0a
DaanVerschraegen <daan.verschraegen@outlook.com>
2021-08-14 17:56:26 +0200

    Added pillar that moves with ground, point gets added after succesfully going trough pipes

--- Assets/MyAssets/Scripts/GameMaster.cs
+++ Assets/MyAssets/Scripts/GameMaster.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameMaster : MonoBehaviour
 {
     public static GameMaster instance;
     public float movingSpeed = -1.5f;
+
+    private int score = 0;
+    [SerializeField] private TextMeshProUGUI txtScore; 
     
     private void Awake()
     {
@@ -25,6 +29,13 @@ public class GameMaster : MonoBehaviour
     {
         //Show add
         //After add reload scene:
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //Add 1 score point per column flown trough
+    public void AddScore()
+    {
+        score++;
+        txtScore.text = score.ToString();
     }
 }
